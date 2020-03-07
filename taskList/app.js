bba0e6a80aa1076eac6b7575544d3b32d9f55aed
kevinhtran <kevinhtran@gmail.com>
2020-03-06 16:47:17 -0800

    defined UI variables

---
+++ taskList/app.js
@@ -0,0 +1,6 @@
+// Define UI Variables
+const form = document.querySelector('#task-form'); // you can use getElementById if you want // this is the the entire new task, add task, task list form
+const taskList = document.querySelector('.collection'); // this is the ul
+const clearBtn = document.querySelector('.clear-tasks'); // the clear tasks button
+const filter = document.querySelector('#filter'); // where you input a filter task
+const taskInput = document.querySelector('#task'); // where you input a new task
