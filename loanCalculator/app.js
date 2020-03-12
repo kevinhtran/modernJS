77a433802b887ed6cd69509c729a407fd2d4d70e
kevinhtran <kevinhtran@gmail.com>
2020-03-12 01:04:34 -0700

    created a form submit for caluculate button

---
+++ loanCalculator/app.js
@@ -0,0 +1,12 @@
+// Section 4: Loan Calculator
+// [Part 2] - Calculate & Error
+
+// Listen for submit
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+// Calculate Results
+function calculateResults(e){
+  console.log('Calculating...');
+
+  e.preventDefault();
+}
