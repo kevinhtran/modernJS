c717fdc07485f392a768cd524b393da46c4cb3ab
kevinhtran <kevinhtran@gmail.com>
2020-03-12 01:56:27 -0700

    created a function to alert an error when inputs to forms are incorrect and created a clear error function

--- loanCalculator/app.js
+++ loanCalculator/app.js
@@ -32,8 +32,33 @@ function calculateResults(e){
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
   } else {
-    console.log('Please check your numbers');
+    showError('Please check your numbers')
   }
 
   e.preventDefault();
+}
+
+// Show Error
+function showError(error) {
+  const errorDiv = document.createElement('div');
+
+  // Get elements
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
+  // Add bootstrap class
+  errorDiv.className = 'alert alert-danger';
+
+  // Create text node and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // Insert error above heading
+  card.insertBefore(errorDiv, heading);
+
+  // Clear error after 3 seconds
+  setTimeout(clearError, 3000); // 3000 milliseconds is 3 seconds
+}
+// Clear error
+function clearError() {
+  document.querySelector('.alert').remove();
 }
