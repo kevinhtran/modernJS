ab48352b2adc26775d4fccacc128d0f2f497e22d
kevinhtran <kevinhtran@gmail.com>
2020-03-12 01:45:35 -0700

    created a calculation to compute monthly payment

--- loanCalculator/app.js
+++ loanCalculator/app.js
@@ -6,22 +6,34 @@ document.getElementById('loan-form').addEventListener('submit', calculateResults
 
 // Calculate Results
 function calculateResults(e){
-  console.log('Calculating...');
+  // console.log('Calculating...');
 
   // UI Vars
   // we need all of our form fields
-  const UIamount =  document.getElementById('amount');
-  const UIinterest = document.getElementById('interest');
-  const UIyears = document.getElementById('years');
-  const UImonthlypayment = document.getElementById('monthly-payment');
-  const UItotalpayment = document.getElementById('total-payment');
-  const UItotalinterest = document.getElementById('total-interest');
+  const amount =  document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
 
   // parseFloat() will turn it into a decimal
   // Calculation/Formulas for principal, interest, and payments 
   const principal = parseFloat(amount.value);
   const calculatedInterest = parseFloat(interest.value) / 100 / 12;
   const calculatedPayments = parseFloat(years.value) * 12;
+
+  // Compute monthly payment
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+  if(isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+  } else {
+    console.log('Please check your numbers');
+  }
 
   e.preventDefault();
 }
