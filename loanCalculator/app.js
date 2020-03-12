9cfd6f95e3c9b6361119063bf4054b11c20991d3
kevinhtran <kevinhtran@gmail.com>
2020-03-12 01:14:01 -0700

    created UI variables for form fields

--- loanCalculator/app.js
+++ loanCalculator/app.js
@@ -8,5 +8,15 @@ document.getElementById('loan-form').addEventListener('submit', calculateResults
 function calculateResults(e){
   console.log('Calculating...');
 
+  // UI Vars
+  // we need all of our form fields
+  const UIamount =  document.getElementById('amount');
+  const UIinterest = document.getElementById('interest');
+  const UIyears = document.getElementById('years');
+  const UImonthlypayment = document.getElementById('monthly-payment');
+  const UItotalpayment = document.getElementById('total-payment');
+  const UItotalinterest = document.getElementById('total-interest');
+
+
   e.preventDefault();
 }
