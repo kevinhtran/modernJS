f333648f0dafd93da375c1bdef192e91fcd55035
kevinhtran <kevinhtran@gmail.com>
2020-03-13 03:56:30 -0700

    assigned variables for game values and UI elements

--- numberGuesser/app.js
+++ numberGuesser/app.js
@@ -1,2 +1,33 @@
 // Section 4: Number Guesser Project
-// [Part 1] - Build The UI
+// [Part 2] - Validation & Winning Case
+
+/* 
+GAME FUNCTION:
+- Player must guess a number between a min and max
+- Player gets a certain amount of guesses
+- Notify player of guesses remaining
+- Notify the player of the correct answer if loose
+- Let player choose to play again
+*/
+
+// Game values
+// notice this convention of defining variables
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+// UI elements
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-btn'),
+      message = document.querySelector('.message')
+
+// Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+
+
