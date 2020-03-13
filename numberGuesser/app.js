23e38f36e5802cf764d924e9d34ddff35ee3674f
kevinhtran <kevinhtran@gmail.com>
2020-03-13 04:25:49 -0700

    created a conditional to check to see if it is the winning number

--- numberGuesser/app.js
+++ numberGuesser/app.js
@@ -22,7 +22,7 @@ const game = document.querySelector('#game'),
       minNum = document.querySelector('.min-num'),
       maxNum = document.querySelector('.max-num'),
       guessBtn = document.querySelector('#guess-btn'),
-      guessInput = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
       message = document.querySelector('.message')
 
 // Assign UI min and max
@@ -30,11 +30,23 @@ minNum.textContent = min;
 maxNum.textContent = max;
 
 // Listen for guess
-guessBtn.addEventListener('click', function() {
-  let guess = parseInt(console.log(guessInput.value));
+guessBtn.addEventListener('click', function(){
+  let guess = parseInt(guessInput.value);
   // Validate
   if(isNaN(guess) || guess < min || guess > max) {
     setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+  }
+
+  // Check if won
+  if (guess === winningNum) {
+    // Disable input
+    guessInput.disabled = true;
+    // Change border color
+    guessInput.style.borderColor = 'green';
+    // Set message
+    setMessage(`${winningNum} is correct!, you WIN!`, 'green');
+  } else {
+    
   }
 });
 
