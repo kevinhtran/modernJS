9438e57bf9c3a916bec352586439a9c017a2dea9
kevinhtran <kevinhtran@gmail.com>
2020-03-16 23:55:46 -0700

    created play again event listener

--- numberGuesser/app.js
+++ numberGuesser/app.js
@@ -29,6 +29,13 @@ const game = document.querySelector('#game'),
 minNum.textContent = min;
 maxNum.textContent = max;
 
+// Play again event listener
+game.addEventListener('mousedown', function(e){
+  if(e.target.className === 'play-again'){
+    window.location.reload();
+  }
+});
+
 // Listen for guess
 guessBtn.addEventListener('click', function () {
   let guess = parseInt(guessInput.value);
