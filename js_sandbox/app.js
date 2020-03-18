f201c07d64bf404cbd16a0e40a2aaad3123818b8
kevinhtran <kevdeveloper7@gmail.com>
2020-03-18 03:10:25 -0700

    working with the basis of classes in ES6

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,32 +1,37 @@
 // Section 5: Object Oriented JavaScript - ES5 - ES2015(ES6)
-// Using Object.create
+// ES6 Classes
+// Classes are considered syntactic sugar or convenience syntax because we're just changing the way we write them and not the way that it works under the hood
 
- const personPrototypes = {
-   greeting: function() {
-     return `Hello there ${this.firstName} ${this.lastName}`
-   },
-   getsMarried: function(newLastName) {
-     this.lastName = newlastName;
-   }
- }
+class Person {
+  constructor(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthday = new Date(dob);
+  }
 
- const mary = Object.create(personPrototypes);
-mary.firstName = 'Mary';
-mary.lastName = 'Williams';
-mary.age = 30;
+  greeting() {
+      return  `Hello there ${this.firstName} ${this.lastName}`;
+  }
+
+  calculateAge() {
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
+
+  getsMarried(newLastName) {
+    this.lastName = newLastName;
+  }
+
+  static addNumbers(x, y) { // static method. standalone functions
+    return x + y;
+  }
+}
+
+const mary = new Person('Mary', 'Williams', '11-13-1980');
 
 mary.getsMarried('Thompson');
 
 console.log(mary.greeting());
-
-const brad = Object.create(personPrototypes, {
-  firstName: {value: "Brad"},
-  lastName: {value: "Traversy"},
-  age: {value: 36}
-});
-
-console.log(brad);
-
-console.log(brad.greeting());
-
-// this is an alternative way to create object using Object.create method
+console.log(mary.calculateAge());
+console.log(Person.addNumbers(1,2));
