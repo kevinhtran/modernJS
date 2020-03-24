28114f3aa4170828e3aff0c8ee9e2e10c6879107
kevinhtran <kevdeveloper7@gmail.com>
2020-03-23 23:49:13 -0700

    working with callback functions to mimic actions of creating a blog post on a server/API

--- ajax_sandbox/app.js
+++ ajax_sandbox/app.js
@@ -1,37 +1,54 @@
 // Section 7: Asynchronous JavaScript, Ajax, & Fetch API
-// [Part 5]: Date From an External API - Chuck Norris Project
+// [Part 6]: Callback Functions
+// A callback is a function that's passed in as a parameter to another function and then it's ran inside the function body
 
 // Objective:
-// Fetch data from an external API using Ajax
-// Using a simple API that takes a GET request formatted as a certain URL and return random Chuck Norris Jokes (lol)
+// Create a callback function and see what they do behind the scenes
+// mimic the actions of creating a blog post on a server or through an API/database
 
 
-document.querySelector('.get-jokes').addEventListener('click', getJokes);
+const posts = [
+  {title: 'Post One', body: 'This is post one'},
+  {title: 'Post Two', body: 'This is post two'}
+];
 
-function getJokes(e) {
-  const number = document.querySelector('input[type="number"]').value;
+// function createPost() {
+//   setTimeout(function() {
+//     posts.push(post);
+//   }, 2000);
+// }
 
-  const xhr = new XMLHttpRequest();
 
-  xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+// function getPosts() {
+//   setTimeout(function() {
+//     let output = '';
+//     posts.forEach(function(post) {
+//       output += `<li>${post.title}</li>`;
+//     });
+//     document.body.innerHTML = output;
+//   }, 1000);
+// }
 
-  xhr.onload = function() {
-    if(this.status === 200) {
-      const response = JSON.parse(this.responseText);
-      
-      let output = '';
+// createPost({title: 'Post Three', body: 'This is post three'});
 
-      if(response.type === 'success') {
-        response.value.forEach(function(joke) {
-          output += `<li>${joke.joke}</li>`;
-        });
-      } else {
-        output += '<li>Something went wrong</li>'; // append using +=
-      }
+// getPosts();
 
-      document.querySelector('.jokes').innerHTML = output;
-    }
-  }
-  xhr.send();
-  e.preventDefault();
+function createPost(post, callback) {
+  setTimeout(function() {
+    posts.push(post);
+    callback();
+  }, 2000);
 }
+
+
+function getPosts() {
+  setTimeout(function () {
+    let output = '';
+    posts.forEach(function(post) {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+}
+
+createPost({ title: 'Post Three', body: 'This is post three' }, getPosts);
