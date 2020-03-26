d4ff84b5ec8a65a2c7b8e9eac4680e41d6d78726
kevinhtran <kevdeveloper7@gmail.com>
2020-03-26 02:31:55 -0700

    grabbed button 2 and added an event listener with a method that gets the local JSON data

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -4,9 +4,13 @@
 // Objective: 
 // Recreate the easy HTTP library using Fetch API and some updated standards of ES6 instead of using prototypes.
 
-// Grab the button 1 ID
+// Grab the button 1 ID that links to the text file
 document.getElementById('button1').addEventListener('click', getText);
 
+// Grab the button 2 ID that links to the JSON file
+document.getElementById('button2').addEventListener('click', getJson);
+
+// get local text file data
 function getText() {
   fetch('test.txt')
   .then(function(res) {
@@ -19,4 +23,23 @@ function getText() {
   .catch(function(err) {
     console.log(err)
   })
+}
+
+// get local JSON data
+function Json() {
+  fetch('post.json')
+    .then(function (res) {
+      return res.json();
+    }); // fetch returns promises
+  .then(function (data) { // what we have here is a promise within a promise
+      console.log(data);
+      let output = '';
+      data.forEach(function(post) {
+        output += `<li>${post.title}</li>`
+      });
+      document.getElementById('output').innerHTML = output;
+    })
+    .catch(function (err) {
+      console.log(err)
+    })
 }
