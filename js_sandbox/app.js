b2844ad24dba644e421e5d60397cdd3c562e952f
kevinhtran <kevdeveloper7@gmail.com>
2020-03-26 02:02:48 -0700

    created a callback function

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,36 +1,29 @@
-// Section 5: Object Oriented JavaScript - ES5 - ES2015(ES6)
-// Sub Classes - Inheritence in ES6 classes
+// Section 7:  Asynchronous JavaScript, Ajax & Fetch API
+// ES6 Promises
 
-// class that you're creating
-class Person {
-  constructor(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
+// Objective:
 
-  greeting() {
-    return `Hello there ${this.firstName} ${this.lastName}`
-  }
+
+const posts = [
+  { title: 'Post One', body: 'This is post one' },
+  { title: 'Post Two', body: 'This is post two' }
+];
+
+function createPost(post) {
+  setTimeout(function(){
+    posts.push(post);
+    callback();
+  }, 2000);
 }
 
-// class that you're extending
-class Customer extends Person {
-  constructor(firstName, lastName, phone, membership) {
-    super(firstName, lastName); // calls the parent class constructor, you have to pass in whatever parameters that was in the parent class and anything extra is just in this sub class
-
-    this.phone = phone;
-    this.membership  = membership;
-  }
-
-  static getMembershipCost() {
-    return 500;
-  }
+function getPosts() {
+  setTimeout(function () {
+    let output = '';
+    posts.forEach(function (post) {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
 }
 
-// instantiate a new customer
-const john = new Customer('John', 'Doe', '555-555-5555');
-
-console.log(john);
-console.log(john.greeting); 
-console.log(Customer.getMembershipCost()); // Since it's a static method, you use the actual class name. It's only one way because you didn't extend customer, you extended person.
-
+createPost({ title: 'Post Three', body: 'This is post three' }, getPosts);
