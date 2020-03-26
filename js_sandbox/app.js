34409e9ba33ed0a06b5f74571721e8c39e3d2702
kevinhtran <kevdeveloper7@gmail.com>
2020-03-26 03:49:16 -0700

    using es6 arrow functions to clean up code

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -16,52 +16,40 @@ document.getElementById('button3').addEventListener('click', getExternal);
 // get local text file data
 function getText() {
   fetch('test.txt')
-  .then(function(res) {
-    return res.text();
-  }); // fetch returns promises
-  .then(function(data) { // what we have here is a promise within a promise
-    console.log(data);
-    document.getElementById('output').innerHTML = data;
-  })
-  .catch(function(err) {
-    console.log(err)
-  })
-}
+    .then(res => res.text); // es6 way
+    .then(data => { // es6 way
+      console.log(data);
+      document.getElementById('output').innerHTML = data;
+    });
+    .catch(err => console.log(err));
+};
 
 // get local JSON data
 function getJson() {
   fetch('post.json')
-    .then(function (res) {
-      return res.json();
-    }); // fetch returns promises
-  .then(function (data) { // what we have here is a promise within a promise
+    .then(rest => res.json()); // fetch returns promises
+    .then(data => { // what we have here is a promise within a promise
       console.log(data);
       let output = '';
       data.forEach(function(post) {
         output += `<li>${post.title}</li>`
       });
       document.getElementById('output').innerHTML = output;
-    })
-    .catch(function (err) {
-      console.log(err)
-    })
-}
+    });
+    .catch(err => console.log(err));
+};
 
 // get from external API
 function getExternal() {
   fetch('https://api.github.com/users')
-    .then(function (res) {
-      return res.json();
-    }); // fetch returns promises
-  .then(function (data) { // what we have here is a promise within a promise
+    .then(res => res.json); // fetch returns promises
+    .then(data => { // what we have here is a promise within a promise
       console.log(data);
       let output = '';
       data.forEach(function (user) {
         output += `<li>${user.login}</li>`;
       });
       document.getElementById('output').innerHTML = output;
-    })
-    .catch(function (err) {
-      console.log(err)
-    })
+    });
+    .catch(err => console.log(err));
 }
