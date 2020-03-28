56709a153257a684ad60e7d4ba5e7ffda3bf8f73
kevinhtran <kevdeveloper7@gmail.com>
2020-03-28 14:14:51 -0700

    relocate files to easy_http3 project

---
+++ easy_http3/app.js
@@ -0,0 +1,6 @@
+// Section 7: Asynchronous JavaScript, Ajax & Fetch API
+// Custom HTTP Library (Fetch With Async Await) - Part 4
+
+// Objective:
+// Take what we did with Easy HTTP 2 and implement with Async & Await
+
