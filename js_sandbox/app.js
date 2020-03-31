76ff9a79298f9645aeb26bb03d34da2af6b4ed73
kevinhtran <kevdeveloper7@gmail.com>
2020-03-31 00:15:55 -0700

    worked with Metachracter Symbols for regular expressions

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,43 +1,36 @@
 // Section 9: Error Handling & Regular Expressions
-// Regular Expression [Part 1] - Evaluation Functions
+// [Part 2]: Metachracter Symbols
 
-// A regular expression is basically used to describe a pattern of characters
-// Used to pattern matching or searching.
-// Commonly used for validation as well as pulling things out of a body of text or a body of characters.
-// Email address, phone numbers, social security, things that have certain patterns in their formatting
+// Objective: Create a function that will basically console log and say whatever we put in contains the regular expression
 
 let re;
+// Literal Characters
 re = /hello/;
-re = /hello/i; // i = case insensitive
-// re = /hello/g; // Global search
+re = /hello/i;
 
+// Metacharacter Symbols
+re = /^h/i; // Must start with
+re = /world$/i; // Must end with
+re = /^hello$/i; // Must begin and end with
+re = /h.llo/i; // Matches any ONE character
+re = /h*llo/i; // Matches any character 0 or more times
 
-console.log(re); // => /hello/
-console.log(re.source); // => hello
+re = /gre?a?y/i; // Optional character
+re = /gre?a?y?/i; // Escape character
 
-exec() - Return result in an array or null
-const result = re.exec('hello world');
+// String to match
+const str = '';
 
-console.log(result);
-console.log(result[0]); // hello
-console.log(result.index); // 0
-console.log(result.input); // hello world
-
-// test() - Returns true or false
-const result = re.test('Hello');
-console.log(result);
-
-match() - Return result array or null
-const str = 'Hello There';
-const result = str.match(re);
+// Log Results
+const result = re.exec(str);
 console.log(result);
 
-serach() - Returns index of the first match if not found returns -1
-const str = 'Hello There';
-const result = str.search(re);
-console.log(result);
+function reTest(re, str) {
+  if(re.test(str)) {
+    console.log(`${str} matches ${re.source}`);
+  } else {
+    console.log(`${str} does NOT Match ${re.source}`);
+  }
+}
 
-replace() - Return new string with some or all matches of a pattern
-const str = 'Hello There';
-const newStr = str.replace(re, 'Hi');
-console.log(newStr);
+reTest(re, str);
