c2350c0c4f92ef737c07d918684e8d72183f98de
kevinhtran <kevdeveloper7@gmail.com>
2020-04-01 00:56:07 -0700

    working with es6 symbols

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,58 +1,32 @@
 // Section 10: Other Newer Features - ES2015+
-// Iterators & Generators
-// Part of ES6
-// Iterators - Advance loops that can be paused
-// Generators - Functions that can be paused and return multiple values
+// Symbols
 
-// Iterator Example
-function nameIterator() {
-  let nextIndex = 0;
+// Create a symbol
+const sym1 = new Symbol();
+const sym2 = new Symbol('sym2');
 
-  return {
-    next: function() {
-      return nextIndex < names.length ? { value: names[nextIndex++], done: false} : { done: true}
-    }
-  }
+console.log(Symbol() === Symbol('123'));
+console.log(`Hello ${sym1.toString()}`);
+
+// Unique Object Keys
+const KEY1 = Symbol();
+const KEY2 = Symbol('sym2');
+
+const myObj = {};
+
+myObj[KEY1] = 'Prop1';
+myObj[KEY2] = 'Prop2';
+myObj.key3 = 'Prop3';
+myObj.key4 = 'Prop4';
+
+console.log(myObj[KEY1]);
+console.log(myObj[KEY2]);
+
+// Symbols are not enumerable in for ... in
+for(let i in myObj) {
+  console.log(`${i}: ${myObj[i]}`);
 }
 
-// Create an array of names
-const namesArr = ['Jack', 'Jill', 'John'];
-// Init Iterator and pass in the names array
-const names = nameIterator(namesArr);
-
-console.log(names.next().value);
-console.log(names.next().value);
-console.log(names.next().value);
-console.log(names.next().value);
-
-Generator Example
-function sayNames() {
-  yeild 'Jack';
-  yield 'Jill';
-  yield 'John';
-}
-
-const name = sayNames();
-
-console.log(name.next().value);
-console.log(name.next().value);
-console.log(name.next().value);
-console.log(name.next().value);
-
-// ID Creator
-function* createIds() {
-  let index = 0;
-
-  while(true) {
-    yield index++;
-  }
-}
-
-const gen = createIds();
-
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
+// Symbols are ignored by JSON.stringify
+console.log(JSON.stringify({key: 'prop'}));
+console.log(JSON.stringify({[Symbol('sym1')]: 'prop'}));
