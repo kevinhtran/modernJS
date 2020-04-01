0104c5615bc91ff77c2af57782c2764fadfb2c7f
kevinhtran <kevdeveloper7@gmail.com>
2020-04-01 02:31:37 -0700

    working with the singleton pattern

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,62 +1,34 @@
 // Section 11: JavaScript Patterns
-// Module & Revealing Module Pattern
+// Singleton Pattern
 
-// Basic Structure
-// (function() {
-//   // Declare private vars and functions
+// a manifestation of the module pattern a singleton object is an immediate anonymous function and it can only return one instance of an object at a time.
+// only return one instance of an object at a time.
+//so repeated calls to the constructor will just return the same instance and like the module pattern it maintains a private reference which nothing from the outside can access.
+// Now an example of why you may want to use a singleton is maybe you only want for instance one user object created at a time maybe a logged in user.It would prevent you from having two users from being created at once.
+// oftentimes singletons are frowned upon because they give you a global point of access rather than embracing encapsulation and that bothers a lot of programmers and that they can also be kind of hard to debug
 
-//   return {
-//     // Declare public var and functions
-//   }
-// })();
 
-// STANDARD MODULE PATTERN
-const UICtrl = (function() {
-  let text = 'Hello World';
+const Singleton = (function() {
+  let instance;
 
-  const changeText = function() {
-    const element = document.querySelector('h1');
-    element.textContent = text;
+  function createInstance() {
+    const object =  new Object({name:'Kevin'});
+    return object;
   }
 
   return {
-    callChangeTest: function() {
-      changeText();
-      console.log(text);
+    getInstance: function() {
+      if(!instance){
+        instance = createInstance();
+      }
+      return instance;
     }
   }
 })();
 
-UICtrl.callChangeText();
-  UICtrl.changeText();
+const instanceA = Singleton.getInstance();
+const instanceB = Singleton.getInstance();
 
-Console.log(UICtrl.text);
+console.log(instance === instanceB);
 
-
-// REVEALING MODULE PATTERN
-const ItemCtrl = (function() {
-  let data = [];
-
-  function add(item) {
-    data.push(item);
-    console.log('Item Added...');
-  }
-
-  function get(id) {
-    return data.find(item => {
-      return item.id === id;
-    });
-  }
-
-  return {
-    add: add,
-    // get: get
-  }
-})();
-
-ItemCtrl.add({id: 1, name:  'John'});
-ItemCtrl.add({id: 2, name: 'Mark'});
-console.log(ItemCtrl.get(2));
-
-
-//So that's one of the main differences between the revealing module and the standard module pattern is that we're mapping or we're returning an object literal that directly reveals methods that are inside of the module
+// console.log(instanceA);
