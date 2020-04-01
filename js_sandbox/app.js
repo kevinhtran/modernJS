f5d769a5f042b9e0fe274c1b9f5e74f737d56b5f
kevinhtran <kevdeveloper7@gmail.com>
2020-04-01 03:38:41 -0700

    working with mediator patterns

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,45 +1,55 @@
 // Section 11: JavaScript Patterns
-// Observer Pattern
+// Mediator Pattern
 
-function EventObserver() {
-  this.observers = [];
+const User = function(name) {
+  this.name = name;
+  this.chatroom = null;
+ 
 }
 
-EventObserver.prototype = {
-  subscribe: function(fn) {
-    this.observers.push();
-    console.log('You are now subscribed to ${fn.name}'); 
+User.prototype = {
+  send: function(message, to) {
+    this.chatroom.send(message, this.to);
   },
-  unsubscribe: function(fn) {
-    this.observers = this.observers.filter(function() {
-      if(item !== fn) {
-        return item;
-      }
-    });
-    console.log('You are now unsubscribed from ${fn.name}');
-  },
-  fire: function() {
-    this.observers.forEach(function(item) {
-      item.call();
-    });
+  receive: function(message, from) {
+    console.log(`${from.name} to ${this.name}: ${message}`);
   }
 }
 
-const click = new EventObserver();
+const Chatroom = function() {
+  let users = {}; // list of users
 
-// Event Listeners
-document.querySelector('.sub-ms').addEventListener('click', function() {
-  click.subscribe(getCurMilliseconds);
-});
-
-document.querySelector('.unsub-ms').addEventListener('click', function () {
-  click.unsubscribe(getCurMilliseconds);
-});
-
-document.querySelector('.fire').addEventListener('click', function () {
-  click.fire();
-});
-// Click Handler
-const getCurMilliseconds = function() {
-  console.log(`Current Milliseconds: ${new Date().getMilliseconds()}`);
+  return {
+    register: function(user) {
+      users[user.name] = user;
+      user.chatroom = this;
+    },
+    send: function(messsage, from, to) {
+      if(to) {
+        // single user message
+        to.receive(message, from);
+      } else {
+        // mass message
+        for(key in users) {
+          if(users[key] !== from) {
+            users[key].receive(message,from);
+          }
+        }
+      }
+    }
+  }
 }
+
+const brad = new User('Brad');
+const brad = new User('Jeff');
+const brad = new User('Kevin');
+
+const chatroom = new Chatroom();
+
+chatroom.resgister(brad);
+chatroom.resgister(jeff);
+chatroom.resgister(kevin);
+
+brad.send('Hello Kevin', kevin);
+kevin.send('Hello Brad, you are cool', brad);
+jeff.send('Hello everyone', brad, jeff, kevin);
